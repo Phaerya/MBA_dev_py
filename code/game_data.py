d78e3feb56e1b2420e9e616c6ee062d21635b84e
Kecenne <Kecenne@gmail.com>
2023-09-13 15:12:22 +0200

    Changement Menu Principal / Disposition planètes

--- code/game_data.py
+++ code/game_data.py
@@ -8,7 +8,7 @@ level_0 = {
 		'constraints':'../levels/0/level_0_constraints.csv',
 		'player': '../levels/0/level_0_player.csv',
 		'grass': '../levels/0/level_0_grass.csv',
-		'node_pos': (110,400),
+		'node_pos': (600,352),
 		'unlock': 1,
 		'node_graphics': '../graphics/overworld/0'}
 level_1 = {
@@ -21,7 +21,7 @@ level_1 = {
 		'constraints':'../levels/1/level_1_constraints.csv',
 		'player': '../levels/1/level_1_player.csv',
 		'grass': '../levels/1/level_1_grass.csv',
-		'node_pos': (300,220),
+		'node_pos': (318,623),
 		'node_graphics': '../graphics/overworld/1',
 		'unlock': 2}
 level_2 = {
@@ -34,7 +34,7 @@ level_2 = {
 		'constraints':'../levels/2/level_2_constraints.csv',
 		'player': '../levels/2/level_2_player.csv',
 		'grass': '../levels/2/level_2_grass.csv',
-		'node_pos': (480,610),
+		'node_pos': (892,623),
 		'node_graphics': '../graphics/overworld/2',
 		'unlock': 3}
 level_3 = {
@@ -47,7 +47,7 @@ level_3 = {
 		'constraints':'../levels/2/level_2_constraints.csv',
 		'player': '../levels/2/level_2_player.csv',
 		'grass': '../levels/2/level_2_grass.csv',
-		'node_pos': (610,350),
+		'node_pos': (140,309),
 		'node_graphics': '../graphics/overworld/3',
 		'unlock': 4}
 level_4 = {
@@ -60,7 +60,7 @@ level_4 = {
 		'constraints':'../levels/2/level_2_constraints.csv',
 		'player': '../levels/2/level_2_player.csv',
 		'grass': '../levels/2/level_2_grass.csv',
-		'node_pos': (880,210),
+		'node_pos': (1070,309),
 		'node_graphics': '../graphics/overworld/4',
 		'unlock': 5}
 level_5 = {
@@ -73,7 +73,7 @@ level_5 = {
 		'constraints':'../levels/2/level_2_constraints.csv',
 		'player': '../levels/2/level_2_player.csv',
 		'grass': '../levels/2/level_2_grass.csv',
-		'node_pos': (1050,400),
+		'node_pos': (650,115),
 		'node_graphics':'../graphics/overworld/5',
 		'unlock': 5}
 
