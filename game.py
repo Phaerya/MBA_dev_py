ed0affe91845ad09beb57405e82cd803604db202
SOREAU BASTIEN <80333079+babasssss@users.noreply.github.com>
2023-09-13 11:42:54 +0200

    first commit

--- game.py
+++
@@ -1,7 +0,0 @@
-import pygame
-from robot import Robot
-pygame.init()
-
-class Game:
-    def __init__(self, largeur_fenetre, hauteur_fenetre ):
-        self.robot = Robot(largeur_fenetre, hauteur_fenetre)
