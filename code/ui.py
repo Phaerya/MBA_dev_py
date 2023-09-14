13af00ee7b8c7c2db5713989a8b47bce3b94315e
SOREAU BASTIEN <80333079+babasssss@users.noreply.github.com>
2023-09-14 11:30:41 +0200

    add bubulle bububububule

--- code/ui.py
+++ code/ui.py
@@ -17,12 +17,16 @@ class UI:
 		self.coin_rect = self.coin.get_rect(topleft = (50,61))
 		self.font = pygame.font.Font('../graphics/ui/ARCADEPI.ttf',30)
 
+
+
 	def show_health(self,current,full):
 		self.display_surface.blit(self.health_bar,(20,10))
 		current_health_ratio = current / full
 		current_bar_width = self.bar_max_width * current_health_ratio
 		health_bar_rect = pygame.Rect(self.health_bar_topleft,(current_bar_width,self.bar_height))
 		pygame.draw.rect(self.display_surface,'#dc4949',health_bar_rect)
+
+
 
 	def show_coins(self,amount):
 		self.display_surface.blit(self.coin,self.coin_rect)
